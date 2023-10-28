125e5cc5035d467407e789cc518d7f5a1263b052
shigashiyama <mapion.benri@gmail.com>
2023-10-28 15:16:27 +0900

    Add preprocessing scripts for ENE-BCCWJ

---
+++ bin/examples/ene-bccwj/preprocess_ene-bccwj.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Download UD_Japanese-BCCWJ (r2.8) from
+# <https://github.com/UniversalDependencies/UD_Japanese-BCCWJ/archive/refs/tags/r2.8.tar.gz>
+# and extract it to `data/original/UD_Japanese-BCCWJ/r2.8`.
+
+# Obtain GSK2014-A <https://www.gsk.or.jp/catalog/gsk2014-a> and 
+# extract it to `data/gsk-ene`.
+# Generate XML files in `data/original/gsk-ene/bccwj/xml/` using the installation script.
+
+mkdir -p data/processed/gsk-ene-bccwj/json/
+
+python src/datasets/ene_bccwj/convert_xmls_to_json.py \
+       -x data/original/gsk-ene/bccwj/xml \
+       -j data/processed/gsk-ene-bccwj/json/bccwj-exPN-train.json \
+       -i data/supplement/UD_Japanese-BCCWJ/UDJB_train_ids.txt \
+       --exclude_domains PN
+
+python src/datasets/ene_bccwj/convert_xmls_to_json.py \
+       -x data/original/gsk-ene/bccwj/xml \
+       -j data/processed/gsk-ene-bccwj/json/bccwj-exPN-dev.json \
+       -i data/supplement/UD_Japanese-BCCWJ/UDJB_dev_ids.txt \
+       --exclude_domains PN
+
+python src/datasets/ene_bccwj/convert_xmls_to_json.py \
+       -x data/original/gsk-ene/bccwj/xml \
+       -j data/processed/gsk-ene-bccwj/json/bccwj-exPN-test.json \
+       -i data/supplement/UD_Japanese-BCCWJ/UDJB_test_ids.txt \
+       --exclude_domains PN
